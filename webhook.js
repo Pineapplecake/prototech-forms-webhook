594560589ae06b4621fda92fc171d03203bdccd5
Pineapplecake <ecorb@fluidarch.com>
2020-08-29 22:17:57 -0500

    Created webhook.js

---
+++ webhook.js
@@ -0,0 +1,8 @@
+var POST_URL = "Paste your webhook url here";
+
+function onSubmit(e) {
+    var form = FormApp.getActiveForm();
+    var allResponses = form.getResponses();
+    var latestResponse = allResponses[allResponses.length - 1];
+    var response = latestResponse.getItemResponses();
+};
